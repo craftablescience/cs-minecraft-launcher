62facef81a265193be189f820deb065001570eb3
Brendan Lewis <brendanelewis@gmail.com>
2021-05-22 22:00:09 -0400

    Feat: actually add the project

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace CustomMinecraftLauncher.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+using ReactiveUI;
+
+namespace CustomMinecraftLauncher.ViewModels
+{
+    public abstract class ViewModelBase : ReactiveObject
+    {
+    }
+}
